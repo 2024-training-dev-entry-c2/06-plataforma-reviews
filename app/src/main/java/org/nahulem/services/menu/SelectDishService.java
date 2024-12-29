66523039bdbb2acc30cf5bfc9fa948b49e842a43
Nahuel Lemes <nahulem@gmail.com>
2024-12-29 17:03:09 -0300

    feat(services): crear SelectDishService
    Servicio que gestiona la selección de un plato

--- app/src/main/java/org/nahulem/services/menu/SelectDishService.java
+++ app/src/main/java/org/nahulem/services/menu/SelectDishService.java
@@ -1,17 +1,60 @@
 package org.nahulem.services.menu;
 
+import org.nahulem.models.Dish;
+import org.nahulem.models.Restaurant;
 import org.nahulem.repositories.DataRepository;
 import org.nahulem.services.interfaces.ICommand;
+import org.nahulem.services.restaurant.SelectRestaurantService;
+import org.nahulem.utils.Validator;
 
-public class SelectDishService implements ICommand<Boolean> {
+import java.util.List;
+
+public class SelectDishService implements ICommand<Dish> {
+    private final Validator validator;
     private final DataRepository repository;
+    private final SelectRestaurantService selectRestaurantService;
 
-    public SelectDishService(DataRepository repository) {
+    public SelectDishService(DataRepository repository, Validator validator, SelectRestaurantService selectRestaurantService) {
         this.repository = repository;
+        this.validator = validator;
+        this.selectRestaurantService = selectRestaurantService;
     }
 
     @Override
-    public Boolean execute() {
-        return null;
+    public Dish execute() {
+        Restaurant restaurant = selectRestaurantService.execute();
+
+        if (restaurant == null) {
+            return null;
+        }
+
+        List<Dish> dishes = restaurant.getMenu().getDishes();
+
+        if (dishes.isEmpty()) {
+            System.out.println("No hay platos en el menú.");
+            return null;
+        }
+
+        showDishes(dishes);
+        Integer index = getSelectedDish(dishes.size());
+
+        return dishes.get(index);
+    }
+
+    private void showDishes(List<Dish> dishes) {
+        validator.printMessage("Lista de Platos: \n-------------------");
+        dishes.forEach(dish -> validator.printMessage((dishes.indexOf(dish)) + ". " + dish.getName() + "\n" + "-------------------")
+        );
+    }
+
+    private int getSelectedDish(int size) {
+        int index = validator.readInt("Ingresa el número del plato que deseas seleccionar: ");
+
+        if (index < 1 || index > size) {
+            validator.printMessage("El número ingresado no es válido, intenta de nuevo.");
+            return getSelectedDish(size);
+        }
+
+        return index;
     }
 }
